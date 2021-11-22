052733bb2d61c60fa14421b0bf190d1098a5787e
skymill2000 <skymill2000@gmail.com>
2021-11-22 16:05:32 +0900

    javascript object 실습

---
+++ jsExample/exampleObject.js
@@ -0,0 +1,10 @@
+let car = {
+    name : "bmw",
+    ph : 500,
+    start : function() {
+        console.log("engine start");
+    },
+    stop : function(){
+        console.log("stop");
+    }
+}
