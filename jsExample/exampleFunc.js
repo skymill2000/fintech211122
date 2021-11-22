296bc7c625dbb0bd2288cc90fac2a26d6fd2cbb0
skymill2000 <skymill2000@gmail.com>
2021-11-22 16:26:31 +0900

    화살표 함수로 기능 구현 실습

--- jsExample/exampleFunc.js
+++ jsExample/exampleFunc.js
@@ -2,4 +2,22 @@ function plus(p1, p2){
     return p1 + p2 ;
 }
 
+//arrow function
+const minus = (p1, p2) => {
+    return p1 - p2;
+}
+//div , multi 이름으로 arrow func 2개 추가
+
+const multi = (p1, p2) => {
+    return p1 * p2;
+}
+
+const div = (p1, p2) => {
+    return p1 / p2;
+}
+
+
 console.log(plus(3,6));
+console.log(minus(3,6));
+console.log(div(3,6));
+console.log(multi(3,6));
