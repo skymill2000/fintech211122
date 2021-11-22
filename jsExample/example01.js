af6056d719d769b11fca864ba1093cbc5257024a
skymill2000 <skymill2000@gmail.com>
2021-11-22 15:46:35 +0900

    변수 선언

---
+++ jsExample/example01.js
@@ -0,0 +1,11 @@
+// in java
+// private String name = "Gwan woo" <- 1999 => compile error;
+const userName;
+console.log(userName);
+userName = 100
+console.log(userName);
+userName = [122,1323,123123]
+console.log(userName);
+
+// let userName = "hello";
+// console.log(userName);
