a13e5608773da34527050875f8e9f35404557b00
skymill2000 <skymill2000@gmail.com>
2021-11-22 16:11:40 +0900

    자동차 객체 수행

--- jsExample/exampleObject.js
+++ jsExample/exampleObject.js
@@ -5,6 +5,13 @@ let car = {
         console.log("engine start");
     },
     stop : function(){
-        console.log("stop");
+        console.log("engine stop");
     }
 }
+
+//in java
+// class {...} public Car car = new Car();
+console.log(car.name);
+//자동차 오브젝트에 start 를 실행해주세요!
+car.start();
+console.log(car.start);
