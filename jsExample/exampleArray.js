2644560409be0f6b5aca784100a01fd60b1a39af
skymill2000 <skymill2000@gmail.com>
2021-11-22 16:44:50 +0900

    배열 / 반복문

--- jsExample/exampleArray.js
+++ jsExample/exampleArray.js
@@ -2,5 +2,17 @@ let cars = ["bmw", "sonata", "tesla", "tico"];
 // console.log(cars);
 //java public int [] intArray = new int[5];
 
-console.log(cars[0]);
-console.log(cars[3]);
+// console.log(cars[5]);
+// console.log(cars[3]);
+
+for(let i = 0; i < cars.length; i++){
+    console.log(cars[i]);
+}
+console.log("-----of-----")
+for (car of cars){
+    console.log(car)
+}
+console.log("-----map-----")
+cars.map((car) => {
+    console.log(car);
+})
