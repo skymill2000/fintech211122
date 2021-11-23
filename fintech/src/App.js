bad9217d12c43dbf850c86bb5ae1bc514b51e265
skymill2000 <skymill2000@gmail.com>
2021-11-23 16:15:06 +0900

    스타일드 컴포넌트 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,3 +1,4 @@
+import TopHeader from "./components/common/TopHeader";
 import ListExmaple from "./components/ListExmaple";
 import StateExample from "./components/StateExample";
 import Welcome from "./components/Welcome";
@@ -7,6 +8,7 @@ const greeting = <h1>안녕하세요 리액트 입니다.</h1>;
 function App() {
   return (
     <>
+      <TopHeader title="연습장"></TopHeader>
       {greeting}
       <Welcome username="홍길동" userage="30"></Welcome>
       <Welcome username="고길동" userage="36"></Welcome>
---
+++ fintech/src/components/common/TopHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopHeaderBlock = styled.div`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: bold;
+  width: auto;
+  height: 60px;
+  border-bottom: 1px dotted #000;
+`;
+
+const TopHeader = ({ title }) => {
+  return <TopHeaderBlock>{title}</TopHeaderBlock>;
+};
+
+export default TopHeader;
