6619b38f7f73eb12e80518fa3d481671508faad5
skymill2000 <skymill2000@gmail.com>
2021-11-23 15:10:18 +0900

    setState 를 통한 값의 변경

--- fintech/src/components/StateExample.js
+++ fintech/src/components/StateExample.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateExample = () => {
   let username = "홍길동";
-
+  const [stateUsername, setStateUsername] = useState("홍길동"); // 스테이트 선언
   const handleChange = (e) => {
     const { value } = e.target;
-    username = value;
-    console.log(username);
+    // username = value;
+    setStateUsername(value);
   };
 
   return (
     <div>
       state 예제
-      <p>{username}</p>
+      <p>{stateUsername}</p>
       <input onChange={handleChange}></input>
     </div>
   );
