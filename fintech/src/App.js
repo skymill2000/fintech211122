102d19817d8ebf55309142efb23f5d96f2f7dfcf
skymill2000 <skymill2000@gmail.com>
2021-11-23 14:08:22 +0900

    Welcome component 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+const greeting = <h1>안녕하세요 리액트 입니다.</h1>;
+
+const Welcome = () => {
+  return <p>반갑습니다 !</p>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {greeting}
+      <Welcome></Welcome>
+    </>
   );
 }
 
