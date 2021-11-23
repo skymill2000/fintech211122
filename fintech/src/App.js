06771180844a5bf24a457c1fd8baed7340999cbe
skymill2000 <skymill2000@gmail.com>
2021-11-23 16:28:44 +0900

    react router dom 추가 app.js. 파일 라우터 코드 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ListExmaple from "./components/ListExmaple";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/test" exact></Route>
+        <Route path="/test" exact>
+          <ListExmaple></ListExmaple>
+        </Route>
       </Switch>
     </Router>
   );
