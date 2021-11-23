3f1588ac7b66918d0d653dedad612df42956da31
skymill2000 <skymill2000@gmail.com>
2021-11-23 14:17:36 +0900

    compenet 와 props

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,14 +1,21 @@
 const greeting = <h1>안녕하세요 리액트 입니다.</h1>;
 
-const Welcome = () => {
-  return <p>반갑습니다 !</p>;
+const Welcome = (props) => {
+  console.log(props);
+  return (
+    <p>
+      반갑습니다 {props.username} 님 나이는 {props.userage} 세 입니다. !
+    </p>
+  );
 };
+// 컴포넌트
 
 function App() {
   return (
     <>
       {greeting}
-      <Welcome></Welcome>
+      <Welcome username="홍길동" userage="30"></Welcome>
+      <Welcome username="고길동" userage="36"></Welcome>
     </>
   );
 }
