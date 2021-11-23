c9222409276d9fb5b9311172019484a622d5ae98
skymill2000 <skymill2000@gmail.com>
2021-11-23 15:27:21 +0900

    클릭 이벤트 바인딩 예제

--- fintech/src/components/StateExample.js
+++ fintech/src/components/StateExample.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 const StateExample = () => {
-  let username = "홍길동";
   const [stateUsername, setStateUsername] = useState("홍길동"); // 스테이트 선언
   const handleChange = (e) => {
     const { value } = e.target;
@@ -9,11 +8,16 @@ const StateExample = () => {
     setStateUsername(value);
   };
 
+  const handleClick = () => {
+    alert("버튼을 글릭했습니다.");
+  };
+
   return (
     <div>
       state 예제
       <p>{stateUsername}</p>
       <input onChange={handleChange}></input>
+      <button onClick={handleClick}>클릭 테스트</button>
     </div>
   );
 };
