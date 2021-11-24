71bbc32374f609d1ffdb4b2c7bb07275d9390aef
skymill2000 <skymill2000@gmail.com>
2021-11-24 13:32:12 +0900

    api 통신부분 수정

--- fintech/src/pages/NewApiPage.js
+++ fintech/src/pages/NewApiPage.js
@@ -8,14 +8,11 @@ const NewApiPage = () => {
   //스테이트 추가
   const handleClick = () => {
     //템플릿 리터럴을 사용
-    axios
-      .get(
-        "https://newsapi.org/v2/everything?q=apple&from=2021-10-24&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5&language=ko"
-      )
-      .then(({ data }) => {
-        console.log(data);
-        setNewsList(data.articles);
-      });
+    const url = `https://newsapi.org/v2/everything?q=${serachInput}&from=2021-10-24&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5&language=ko`;
+    axios.get(url).then(({ data }) => {
+      console.log(data);
+      setNewsList(data.articles);
+    });
   };
 
   const handleChange = (e) => {
@@ -30,8 +27,8 @@ const NewApiPage = () => {
       <input onChange={handleChange}></input>
       <button onClick={handleClick}>검색</button>
       {/* result list component */}
-      {newsList.map((n) => {
-        return <p>{n.title}</p>;
+      {newsList.map((n, index) => {
+        return <p key={index}>{n.title}</p>;
       })}
     </>
   );
