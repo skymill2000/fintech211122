b974fdc53e8409af8406caaaed45f788531e9ea9
skymill2000 <skymill2000@gmail.com>
2021-11-24 16:27:55 +0900

    사용자 인증 버튼 ㅋ추가

--- fintech/src/pages/AuthPage.js
+++ fintech/src/pages/AuthPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import TopHeader from "../components/common/TopHeader";
 
 const AuthPage = () => {
-  return <div></div>;
+  const openAuthCodePage = () => {
+    let tmpwindow = window.open("about:blank");
+    const clientId = "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF"; //본인것으로 변경해주세요 !
+    tmpwindow.location.href = `https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0`;
+  };
+
+  return (
+    <div>
+      <TopHeader title="사용자 인증"></TopHeader>
+      <button onClick={openAuthCodePage}>사용자 인증</button>
+    </div>
+  );
 };
 
 export default AuthPage;
