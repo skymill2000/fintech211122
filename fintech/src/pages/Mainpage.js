c11b434ace367f69dd7b6f674031134d6ab5d4bf
skymill2000 <skymill2000@gmail.com>
2021-11-25 11:31:50 +0900

    work4 중간 점검

--- fintech/src/pages/Mainpage.js
+++ fintech/src/pages/Mainpage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TopHeader from "../components/common/TopHeader";
-
+import axios from "axios";
 const Mainpage = () => {
   const [accountList, setAccountList] = useState([]);
 
@@ -13,7 +13,18 @@ const Mainpage = () => {
     const accessToken = localStorage.getItem("accessToken");
     const userSeqNo = localStorage.getItem("userSeqNo");
     console.log(accessToken, userSeqNo);
+
     //work4 사용자 정보 조회 Api 호출
+    const option = {
+      method: "POST",
+      url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      params: {},
+    };
+
+    axios(option).then(({ data }) => {});
   };
 
   return (
