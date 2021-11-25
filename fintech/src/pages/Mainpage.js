46ea7c6a34eca7f9f3c49463cddbcd2bcfda3abe
skymill2000 <skymill2000@gmail.com>
2021-11-25 11:20:10 +0900

    work 3) 사용자 정보 조회 api 작성하기

--- fintech/src/pages/Mainpage.js
+++ fintech/src/pages/Mainpage.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopHeader from "../components/common/TopHeader";
 
 const Mainpage = () => {
+  const [accountList, setAccountList] = useState([]);
+
+  useEffect(() => {
+    getAccountList();
+  }, []);
+
+  const getAccountList = () => {
+    //? user/me url 요청 보내기
+    const accessToken = localStorage.getItem("accessToken");
+    const userSeqNo = localStorage.getItem("userSeqNo");
+    console.log(accessToken, userSeqNo);
+    //work4 사용자 정보 조회 Api 호출
+  };
+
   return (
     <>
       <TopHeader title="계좌목록"></TopHeader>
