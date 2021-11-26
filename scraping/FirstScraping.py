e16c092c1fecb0ab7aee06d199e5f7c8228b8497
skymill2000 <skymill2000@gmail.com>
2021-11-26 16:08:26 +0900

    뉴스검색 타이틀 / 소제목

--- scraping/FirstScraping.py
+++ scraping/FirstScraping.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 driver = webdriver.Chrome('./chromedriver')
-driver.get("https://www.etnews.com/20211125000143?mc=ns_001_00001")
-# title = driver.find_element_by_xpath(
-#     '//*[@id="container"]/div[1]/div[1]/div[1]/div[2]/p[1]')
-# description = driver.find_element_by_xpath(
-#     '//*[@id="container"]/div[1]/div[1]/div[1]/div[2]/p[2]')
-# print(title.text)
-# print(description.text)
+driver.get("https://www.jnilbo.com/view/media/view?code=2021082616175071931")
+driver.implicitly_wait(1)
+title = driver.find_element_by_xpath(
+    '//*[@id="container"]/div[1]/div[1]/div[1]/div[2]/p[1]')
+print(title.text)
+subtitle = driver.find_element_by_xpath(
+    '//*[@id="container"]/div[1]/div[1]/div[1]/div[2]/p[2]')
+print(subtitle.text)
